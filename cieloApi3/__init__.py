f3203eae89eb0e8f8373719e04f34ebff251c8f3
Matheus Andrade <matheus.andrade1996@gmail.com>
2021-08-10 14:23:17 -0300

    add settings and dotenv config

--- cieloApi3/__init__.py
+++ cieloApi3/__init__.py
@@ -5,7 +5,7 @@ from .sale import *
 from .customer import *
 from .creditCard import *
 from .payment import *
-
+from .settings import *
 from .recurrentPayment import *
 
 from .cieloEcommerce import *
--- example/saleBoletoSimple.py
+++ example/saleBoletoSimple.py
@@ -11,7 +11,7 @@ import json
 environment = Environment(sandbox=True)
 
 # Configure seu merchant, para gerar acesse: https://cadastrosandbox.cieloecommerce.cielo.com.br/
-merchant = Merchant('MerchantId', 'MerchantKey')
+merchant = Merchant(settings.MERCHANT_ID, settings.MERCHANT_KEY)
 
 # Crie uma instância de Sale informando o ID do pagamento
 sale = Sale('333')
@@ -30,9 +30,9 @@ cielo_ecommerce = CieloEcommerce(merchant, environment)
 
 # Criar a venda e imprime o retorno
 response_create_sale = cielo_ecommerce.create_sale(sale)
-print '----------------------response_create_sale----------------------'
-print json.dumps(response_create_sale, indent=2)
-print '----------------------response_create_sale----------------------'
+print ('----------------------response_create_sale----------------------')
+print (json.dumps(response_create_sale, indent=2))
+print ('----------------------response_create_sale----------------------')
 
 # Com a venda criada na Cielo, já temos o ID do pagamento, TID e demais
 # dados retornados pela Cielo
@@ -40,8 +40,8 @@ payment_id = sale.payment.payment_id
 
 # Com o ID do pagamento, podemos fazer uma consulta do pagamento
 response_get_sale = cielo_ecommerce.get_sale(payment_id)
-print '----------------------response_get_sale----------------------'
-print json.dumps(response_get_sale, indent=2)
-print '----------------------response_get_sale----------------------'
+print ('----------------------response_get_sale----------------------')
+print (json.dumps(response_get_sale, indent=2))
+print ('----------------------response_get_sale----------------------')
 
-print '\r\nLink Boleto:', sale.payment.url, '\r\n'
+print ('\r\nLink Boleto:', sale.payment.url, '\r\n')
